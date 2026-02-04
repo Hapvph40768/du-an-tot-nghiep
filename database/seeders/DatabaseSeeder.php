4733a0ca2e70409a24bd5712d30ea7fd0711959c
Pham van ha <haphamnvb3@gmail.com>
2026-02-04 22:00:05 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,33 +11,7 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        /**
-         * ADMIN mặc định
-         */
-        User::create([
-            'role' => 'admin',
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'phone' => '0900000000',
-            'password' => Hash::make('123456'),
-            'avatar' => null,
-            'status' => 'active',
-        ]);
-
-        /**
-         * STAFF mẫu
-         */
-        User::factory()->count(3)->create([
-            'role' => 'staff',
-        ]);
-
-        /**
-         * CUSTOMER mẫu
-         */
-        User::factory()->count(5)->create([
-            'role' => 'customer',
-        ]);
+    public function run(): void{
+        
     }
 }
